e8699593aa6313fb050b4e2a2547c46bc4d2c81f
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-29 04:32:53 -0300

    buscando dados em um array usando arrow function

--- public/bundle.js
+++ public/bundle.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log('seja bem vindo de volta');
+var todos = [{
+  id: 1,
+  text: 'aprender Javascript',
+  completed: true
+}, {
+  id: 2,
+  text: 'aprender ECMAScript',
+  completed: true
+}, {
+  id: 3,
+  text: 'aprender Vue.js',
+  completed: false
+}];
+var todosList = todos.filter(function (todo) {
+  return todo.completed;
+}); // acima é o mesmo que: const todosList = todos.filter(function (todo) {
+//   return todo.completed;
+// }
+
+console.log(todosList);
+var index = todos.findIndex(function (todo) {
+  return todo.text === 'aprender Vue.js';
+}); // acima estamos buscando o indice do objeto que tem o texto 'aprender Vue.js'
+
+console.log(index);
+var temVue = todos.some(function (todos) {
+  return todos.text === 'aprender Vue.js';
+}); // acima estamos usando o some para verificar se existe algum todo com o texto aprender Vue.js
+
+console.log(temVue);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
-console.log('seja bem vindo de volta');
+const todos = [
+    {
+        id: 1,
+        text: 'aprender Javascript',
+        completed: true
+    },
+    {
+        id: 2,
+        text: 'aprender ECMAScript',
+        completed: true
+    },
+    {
+        id: 3,
+        text: 'aprender Vue.js',
+        completed: false
+    }
+];
+
+const todosList = todos.filter(todo => todo.completed);
+
+// acima é o mesmo que: const todosList = todos.filter(function (todo) {
+//   return todo.completed;
+// }
+console.log(todosList);
+
+const index = todos.findIndex(todo => todo.text === 'aprender Vue.js');
+
+// acima estamos buscando o indice do objeto que tem o texto 'aprender Vue.js'
+
+console.log(index);
+
+const temVue = todos.some(todos => todos.text === 'aprender Vue.js');
+
+// acima estamos usando o some para verificar se existe algum todo com o texto aprender Vue.js
+
+console.log(temVue);
